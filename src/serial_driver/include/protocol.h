a2a1387ad4f1cab5c8434197e413ad0b1e5f479d
phostau628 <468965968@qq.com>
2026-02-03 14:57:17 +0800

    update

--- src/serial_driver/include/protocol.h
+++ src/serial_driver/include/protocol.h
@@ -181,8 +181,25 @@ typedef struct
 } map_data_t;
 #pragma pack()
 
+typedef union {
+    uint8_t buff[16]; 
+    struct {
+        uint8_t SOF;
+        chassis_ctrl_info_t ctrl_data; 
+        uint8_t EOF1;
+        uint8_t EOF2;
+    } __attribute__((packed)) data; 
+} nav_ctrl_t;
 
-
+typedef union {
+    uint8_t buff[51];
+    struct {
+        uint8_t SOF;
+        chassis_odom_info_t odom_data; 
+        uint8_t EOF1;
+        uint8_t EOF2;
+    } __attribute__((packed)) data;
+} nav_odom_t;
 
 
 #pragma pack(pop)
